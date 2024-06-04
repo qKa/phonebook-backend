6011c8b5265ce21100e7f0509aab937cfc61646c
qKa <803318+qKa@users.noreply.github.com>
2024-06-05 00:26:44 +0100

    Phonebook backend step 7 (exercise 3.7)
    - Add morgan middleware for logging HTTP requests
      - Configure morgan to use the 'tiny' format for concise logging
      - Log messages to the console
    
    - Implement handling for unknown endpoints
      - Return a 404 status code with an error message { error: "unknown endpoint" }
    
    This update enhances the application's monitoring and debugging capabilities by adding request logging and handling unknown endpoints.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
-app.use(express.json());
-
-const generateId = () => {
-  const id = Math.floor(Math.random() * 1000000);
-  return id;
-};
 
 let persons = [
   {
@@ -30,6 +24,20 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000000);
+  return id;
+};
+
+app.use(express.json());
+
+// Apply the Morgan middleware
+app.use(morgan("tiny"));
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Phonebook backend</h1>");
@@ -64,7 +72,7 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  console.log("[POST] Request:", body);
+  //   console.log("[POST] Request:", body);
 
   if (!body.name) {
     return response.status(400).json({
@@ -93,7 +101,7 @@ app.post("/api/persons", (request, response) => {
   };
 
   persons = persons.concat(person);
-  console.log(`[POST] New entry ID: '${person.id}' Name: '${person.name}' Number: '${person.number}' created.`);
+  //   console.log(`[POST] New entry ID: '${person.id}' Name: '${person.name}' Number: '${person.number}' created.`);
   response.json(person);
 });
 
@@ -109,6 +117,8 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(404).end();
   }
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
